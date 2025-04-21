f2e2e3d4c38e9e37e8f60de94254268005de200c
Manuel Brizuela <mabrizuela@itba.edu.ar>
2025-04-21 23:36:17 +0000

    nQueens

--- PARCIALES/nQueens.c
+++ PARCIALES/nQueens.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
 
+#define DIRS 4
+
 int nQueens(unsigned int n, char mat[n][n]);
 int amenaza(unsigned int n, char movs[n][n], int fil, int col);
 
 int main(){
+    // Test case 1: Valid 4x4 board with 4 queens
+    char board1[4][4] = {
+        {'0', '1', '0', '0'},
+        {'0', '0', '0', '1'},
+        {'1', '0', '0', '0'},
+        {'0', '0', '1', '0'}
+    };
+    if (!nQueens(4, board1)) return 1;
+
+    // Test case 2: Invalid 4x4 board with queens threatening each other
+    char board2[4][4] = {
+        {'1', '0', '0', '0'},
+        {'0', '1', '0', '0'},
+        {'0', '0', '0', '0'},
+        {'0', '0', '0', '0'}
+    };
+    if (nQueens(4, board2)) return 1;
+
+    // Test case 3: Valid 1x1 board
+    char board3[1][1] = {{'1'}};
+    if (!nQueens(1, board3)) return 1;
+
+    // Test case 4: Invalid board with queens in same row
+    char board4[4][4] = {
+        {'1', '1', '0', '0'},
+        {'0', '0', '0', '0'},
+        {'0', '0', '0', '0'},
+        {'0', '0', '0', '0'}
+    };
+    if (nQueens(4, board4)) return 1;
+
+    // Test case 5: Invalid board with queens in same diagonal
+    char board5[4][4] = {
+        {'1', '0', '0', '0'},
+        {'0', '0', '0', '0'},
+        {'0', '0', '1', '0'},
+        {'0', '0', '0', '0'}
+    };
+    if (nQueens(4, board5)) return 1;
+
+    puts("OK!");
     return 0;
 }
 
@@ -25,9 +68,9 @@ int nQueens(unsigned int n, char mat[n][n]){
 
 int amenaza(unsigned int n, char mat[n][n], int fil, int col){
     static int movs[][2] = {{1, 0}, {0, 1}, {1, -1}, {1, 1}};
-    for (int i = 0; i < sizeof(mat) / sizeof(mat[0]); i++){
+    for (int i = 0; i < DIRS; i++){  // We know there are 4 possible movement directions
         int xAct = col + movs[i][1], yAct = fil + movs[i][0];
-        while (xAct >= 0 && xAct < n && yAct >= 0 && yAct < 0){
+        while (xAct >= 0 && xAct < n && yAct >= 0 && yAct < n){
             if(mat[yAct][xAct] == '1')
                 return 1;
             xAct += movs[i][1];
