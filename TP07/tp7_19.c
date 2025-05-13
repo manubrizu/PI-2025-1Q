eb2076eb76399a9ed54128abf4599e11df2af873
Manuel Brizuela <mabrizuela@itba.edu.ar>
2025-05-13 19:50:25 +0000

    19

---
+++ TP07/tp7_19.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char * copiarPrefijo(char * s, int n);
+
+int main(){
+    char s[] = "Hola";
+    int n = 2;
+
+    char * prefijo = copiarPrefijo(s, n);
+    puts(prefijo);
+
+    free(prefijo);
+    return 0;
+}
+
+char * copiarPrefijo(char * s, int n){
+    char * resp = malloc(n + 1);
+    int i;
+    for (i = 0; i < n && s[i] != 0; i++){
+        resp[i] = s[i];
+    }
+
+    resp[i] = '\0'; 
+
+    return resp;    
+}
