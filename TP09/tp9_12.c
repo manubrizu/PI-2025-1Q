e498bfd44157e3b0b9bcf4982b20543ede6ee708
Manuel Brizuela <mabrizuela@itba.edu.ar>
2025-05-21 20:50:49 +0000

    tp9

---
+++ TP09/tp9_12.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <assert.h>
+
+int balance(const char *s);
+
+int main(void) {
+
+  assert(balance("")==0);
+  assert(balance("((()))")==0);
+  assert(balance(")(")==0);
+  assert(balance("()()")==0);
+  assert(balance(")))(((")==0);
+  assert(balance("))) ((( ))( ()() ()()(")==0);
+  
+  assert(balance(")")!=0);
+  assert(balance("(")!=0);
+  assert(balance(")()")!=0);
+  assert(balance(" ) ")!=0);
+  assert(balance("))))))))))))))))))))))))))))))))))))))))))))))))))))))")!=0);
+  assert(balance("))))))))))))))))))))))))))))))))))))))))))))))))))))))((((((((((((")!=0);
+
+  printf ("OK!\n");
+  return 0;
+}
+
+int balance(const char *s){
+    if(*s=='\0'){
+        return 0;
+    }
+
+    if(*s=='('){
+        return balance(s+1) + 1;
+    }        
+    
+    if(*s==')'){
+        return balance(s+1) - 1;
+    }
+
+    return balance(s+1);
+
+}
