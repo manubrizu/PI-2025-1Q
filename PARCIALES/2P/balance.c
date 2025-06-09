fa89167e5b7c6d554b01262fc7a418286a6926c4
Manuel Brizuela <mabrizuela@itba.edu.ar>
2025-06-09 22:55:59 +0000

    balance

---
+++ PARCIALES/2P/balance.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct docNode {
+    int id;                     // identifica al cliente
+    double amount;              // importe del comprobante (positivo o negativo)
+    struct docNode * tail;
+} docNode;
+
+typedef docNode * docList;
+
+typedef struct balanceNode {
+    int id;                     // identifica al cliente
+    double balance;             // cuánto debe o se le debe al cliente
+    struct balanceNode * tail;
+} balanceNode;
+
+typedef balanceNode * balanceList;
+
+
+
+balanceList balance(docList l1){
+    if(l1 == NULL){
+        return NULL;
+    }
+
+    balanceList lAux = balance(l1->tail);
+    
+    if(lAux == NULL || l1->id != lAux->id){
+        balanceList aux = malloc(sizeof(docNode));
+        aux->id = l1->id;
+        aux->tail = lAux;
+        aux->balance += l1->amount;
+        return aux;
+    }
+    
+    lAux->balance += l1->amount;
+    return lAux;
+}
