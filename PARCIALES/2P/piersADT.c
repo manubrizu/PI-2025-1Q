96c47fa7e91865a7e5951f00d62f053e3388f326
Manuel Brizuela <mabrizuela@itba.edu.ar>
2025-06-09 14:04:44 +0000

    piers

--- PARCIALES/2P/piersADT.c
+++ PARCIALES/2P/piersADT.c
@@ -2,15 +2,11 @@
 #include <stdlib.h>
 #include "piersADT.h"
 
-typedef struct muelle{
-    char amarrado;          // embarcacion amarrada
-    int existsM;           // el muelle existe
-}muelle;
 
 typedef struct puerto{
-    muelle * muelles;
+    char * muelles;
     size_t dim;             // dim del vectorde muelles
-    size_t cantMuelles;     // cantidad de muelles
+    size_t cantBarcos;     // cantidad de muelles
     char existsP;           // el puerto existe
 }puerto;
 
@@ -28,7 +24,7 @@ size_t addPier(piersADT piers, size_t pierNumber){
     if(piers->dim <= pierNumber){
         piers->puertos = realloc(piers->puertos, (pierNumber + 1) * sizeof(puerto));
         for (size_t i = piers->dim; i <= pierNumber; i++){
-            piers->puertos[i].cantMuelles = 0;
+            piers->puertos[i].cantBarcos = 0;
             piers->puertos[i].muelles = NULL;
             piers->puertos[i].dim = 0;
             piers->puertos[i].existsP = 0;
@@ -49,7 +45,7 @@ size_t addPierDock(piersADT piers, size_t pierNumber, size_t dockNumber){
     if(piers->dim <= pierNumber){
         piers->puertos = realloc(piers->puertos, (pierNumber + 1) * sizeof(puerto));
         for (size_t i = piers->dim; i <= pierNumber; i++){
-            piers->puertos[i].cantMuelles = 0;
+            piers->puertos[i].cantBarcos = 0;
             piers->puertos[i].muelles = NULL;
             piers->puertos[i].dim = 0;
             piers->puertos[i].existsP = 0;
@@ -60,29 +56,23 @@ size_t addPierDock(piersADT piers, size_t pierNumber, size_t dockNumber){
     puerto * p = &piers->puertos[pierNumber];
 
     if(p->dim <= dockNumber){
-        p->muelles = realloc(p->muelles, (dockNumber + 1) * sizeof(muelle));
-        for(size_t i = p->cantMuelles; i <= dockNumber; ++i){
-            p->muelles[i].amarrado = 0;
-            p->muelles[i].existsM = 0;
+        p->muelles = realloc(p->muelles, (dockNumber + 1) * sizeof(char *));
+        for(size_t i = p->dim; i <= dockNumber; ++i){
+            p->muelles[i] = 0;
         }
         p->dim = dockNumber + 1;
     }
 
-    if(p->muelles[dockNumber].existsM){
-        return 0;
-    }
-
-    p->muelles[dockNumber].existsM = 1;
-    p->cantMuelles++;
+    p->dim++;
     return 1;
 }
 
 size_t dockShip(piersADT piers, size_t pierNumber, size_t dockNumber){
-    if(piers->dim <= pierNumber || piers->puertos[pierNumber].dim <= dockNumber || ! piers->puertos[pierNumber].muelles[dockNumber].existsM || piers->puertos[pierNumber].muelles[dockNumber].amarrado){
+    if(piers->dim <= pierNumber || piers->puertos[pierNumber].dim <= dockNumber || piers->puertos[pierNumber].muelles[dockNumber]){
         return 0;
     }
 
-    piers->puertos[pierNumber].muelles[dockNumber].amarrado = 1;
+    piers->puertos[pierNumber].muelles[dockNumber] = 1;
     return 1;
 }
 
@@ -91,5 +81,5 @@ int shipInDock(const piersADT piers, size_t pierNumber, size_t dockNumber){
         return -1;
     }
 
-    return piers->puertos[pierNumber].muelles[dockNumber].amarrado;
+    return piers->puertos[pierNumber].muelles[dockNumber];
 }
